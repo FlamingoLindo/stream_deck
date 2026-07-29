bf7b2451d387171b17b8cf234f00534d62dfffb8
flamingo_lindo <vitorantunes2003@gmail.com>
2026-07-29 15:05:19 -0300

    feat: basic buttons

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::time::Duration;
-
 use slint::{Timer, TimerMode};
+use std::time::Duration;
+use windows::Win32::UI::Input::KeyboardAndMouse::{VK_F13, VK_F14, VK_F15, VK_F16, VK_F17};
 
 pub mod components;
 
@@ -15,6 +15,18 @@ fn main() {
         if let Some(window) = weak.upgrade() {
             window.set_current_time(components::clock::clock_logic::get_realtime().into());
         }
+    });
+
+    window.on_key_pressed(move |id| {
+        let vk = match id {
+            2 => VK_F13,
+            3 => VK_F14,
+            4 => VK_F15,
+            5 => VK_F16,
+            6 => VK_F17,
+            _ => return,
+        };
+        components::icon_button::virtual_keys::send_key(vk);
     });
 
     window.run().unwrap();
